58b727a3f8232b2d73a444013cc2088bedc4a818
Midnight-Knight <LTupov2003@yandex.ru>
2022-11-09 01:01:28 +0300

    09 11 22

--- script.js
+++ script.js
@@ -55,3 +55,138 @@ function clock()
 }
 
 clock();
+
+/* section-2 */
+
+let openlupa = false; 
+
+function lupa()
+{
+    if(openlupa == false)
+    {
+        openlupa = true;
+        document.getElementById('lupa').style.width = "30vmin";
+        document.getElementById('lupapoisk').style.width = "22vmin";
+        document.getElementById('lupapoisk').style.paddingLeft = "1vmin";
+        document.getElementById('lupapoisk').style.opacity = "1";
+        document.getElementById('closeLupa').style.width = "4vmin";
+        document.getElementById('closeLupa').style.height = "4vmin";
+        document.getElementById('closeLupa').style.opacity = "1";
+    }
+    else
+    {
+        openlupa = false;
+        document.getElementById('lupa').style.width = "4vmin";
+        document.getElementById('lupapoisk').style.width = "0vmin";
+        document.getElementById('lupapoisk').style.paddingLeft = "0";
+        document.getElementById('lupapoisk').style.opacity = "0";
+        document.getElementById('closeLupa').style.width = "0vmin";
+        document.getElementById('closeLupa').style.height = "0vmin";
+        document.getElementById('closeLupa').style.opacity = "0";
+    }
+}
+
+/* section-3 */
+
+function mouseOn()
+{
+    document.getElementById("cardp").style.opacity = "1";
+    document.getElementById("card").style.opacity = "1";
+}
+
+function mouseOff()
+{
+    document.getElementById("cardp").style.opacity = "0";
+    document.getElementById("card").style.opacity = "0";
+}
+
+let boolmouse = false;
+function mouseClick()
+{
+    if (boolmouse == false)
+    {
+        document.getElementById("cardimg").style.width = "30vmin";
+        document.getElementById("cardimg").style.transform = "translateY(0vmin)";
+        document.getElementById("cardimg").style.height = "50vmin";
+        document.getElementById("cardimg").style.borderRadius = "1vmin";
+        boolmouse = true;
+    }
+    else
+    {
+        document.getElementById("cardimg").style.width = "20vmin";
+        document.getElementById("cardimg").style.transform = "translateY(-10vmin)";
+        document.getElementById("cardimg").style.height = "20vmin";
+        document.getElementById("cardimg").style.borderRadius = "30vmin";
+        boolmouse = false;
+    }
+}
+
+/* section-4 */
+
+
+function s(x1, x2, x3, x4)
+{
+    if (x1 != 9000)
+    {
+        x1+=18;
+        x2+=2;
+        x3+=14;
+        x4+=10;
+        document.getElementById("ys").innerHTML = x1;
+        document.getElementById("ls").innerHTML = x2;
+        document.getElementById("fs").innerHTML = x3;
+        document.getElementById("ts").innerHTML = x4;
+        setTimeout(() => { s(x1,x2,x3,x4); }, 1000);
+    }
+}
+
+s(0,0,0,0);
+
+/* section-5 */
+
+function button()
+{
+    let strEm = document.getElementById("textlog").value;
+    let strPa = document.getElementById("passlog").value;
+    let boollog = false;
+    let boolpas = false;
+    console.log(strEm);
+    if (strEm.length == 0)
+    {
+        document.getElementById('at').innerHTML = "Это полне не может быть пустым";
+        document.getElementById('blocklog').style.border = "0.2vmin solid red";
+        document.getElementById('blocklog').style.boxShadow = "0 0.4vmin red";
+    }
+    else if (strEm.indexOf('@') == -1 || strEm.indexOf('.') == -1 || (strEm.indexOf('@') > strEm.indexOf('.')) || (strEm.indexOf('@') == 0) || ((strEm.indexOf('.')-strEm.indexOf('@')) == 1) || ((strEm.length-strEm.indexOf('.')) == 1))
+    {
+        document.getElementById('at').innerHTML = "Введите правильный адрес";
+        document.getElementById('blocklog').style.border = "0.2vmin solid red";
+        document.getElementById('blocklog').style.boxShadow = "0 0.4vmin red";
+    }
+    else
+    {
+        document.getElementById('at').innerHTML = "";
+        document.getElementById('blocklog').style.border = "0.2vmin solid rgb(122,122,122)";
+        document.getElementById('blocklog').style.boxShadow = "0 0.4vmin rgb(122,122,122)";
+        boollog = true;
+    }
+    
+    if (strPa.length == 0)
+    {
+        document.getElementById('ap').innerHTML = "Это полне не может быть пустым";
+        document.getElementById('blockpas').style.border = "0.2vmin solid red";
+        document.getElementById('blockpas').style.boxShadow = "0 0.4vmin red";
+    }
+    else
+    {
+        document.getElementById('ap').innerHTML = "";
+        document.getElementById('blockpas').style.border = "0.2vmin solid rgb(122,122,122)";
+        document.getElementById('blockpas').style.boxShadow = "0 0.4vmin rgb(122,122,122)";
+        boolpas = true;
+    }
+    
+    if (boollog == true && boolpas == true)
+    {
+        window.open('https://www.google.ru', '_blank');
+    }
+}
